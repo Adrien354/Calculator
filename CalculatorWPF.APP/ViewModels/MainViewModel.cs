1f17172643d5f7fa7fc2fbe2d9c846a3aba02c70
Adrien <adrqwer@outlook.com>
2021-11-29 20:48:40 +0100

    new version of Calculator I have added a square root

--- CalculatorWPF.APP/ViewModels/MainViewModel.cs
+++ CalculatorWPF.APP/ViewModels/MainViewModel.cs
@@ -29,6 +29,7 @@ namespace CalculatorWPF.APP.ViewModels
                 OnPropertyChanged();
             }
         }
+
         public MainViewModel()
         {
             ScreenVal = "0";
@@ -37,12 +38,14 @@ namespace CalculatorWPF.APP.ViewModels
             ClearScreenCommand = new RelayCommand(ClearScreen);
             GetResultCommand = new RelayCommand(GetResult);
             UsingBackspaceCommand = new RelayCommand(UsingBackspace);
+            GetSquareRootCommand = new RelayCommand(GetSquareRoot);
         }
         public ICommand AddNumberCommand { get; set; }
         public ICommand AddOperationCommand { get; set; }
         public ICommand ClearScreenCommand { get; set; }
         public ICommand GetResultCommand { get; set; }
         public ICommand UsingBackspaceCommand { get; set; }
+        public ICommand GetSquareRootCommand { get; set; }
 
         public string number3;
 
@@ -103,6 +106,26 @@ namespace CalculatorWPF.APP.ViewModels
             else
             {
                 ScreenVal = "0";
+            }
+        }
+        private void GetSquareRoot(object obj)
+        {
+            if (isLastSignAnOperation)
+            {
+                MessageBox.Show("The square root of the operation cannot be computed");
+                for (int i = 0; i < ScreenVal.Length - 1; i++)
+                {
+                    char number2 = ScreenVal[i];
+                    char number1;
+                    number3 += number2.ToString();
+                }
+                ScreenVal = number3;
+                number3 = "";
+            }
+            else
+            {
+                var results = Math.Sqrt(Convert.ToDouble(_dataTable.Compute(ScreenVal.Replace(",", "."), "")));
+                ScreenVal = results.ToString();
             }
         }
 
